d0c6984162aac8ace22515cf7677459ed7338146
tangani <tanganimoyo@gmail.com>
2020-09-10 12:00:48 +0200

    service to GET products

---
+++ online-shop/src/app/services/products.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  public getProducts()
+  {
+    let url =  `https://fakestoreapi.com/products`;
+    return this.http.get(url);
+  }
+}
