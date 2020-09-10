6bd705a7b137074fd953918774285d02194cec7d
tangani <tanganimoyo@gmail.com>
2020-09-10 20:21:22 +0200

    updating subfolders

--- valarcci/src/app/dynamic-table/dynamic-table.component.ts
+++ valarcci/src/app/dynamic-table/dynamic-table.component.ts
@@ -1,28 +1,11 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from "@angular/material/table";
 import { SelectionModel, DataSource } from "@angular/cdk/collections";
-import { of, Observable } from "rxjs";
+import {of, Observable, BehaviorSubject} from "rxjs";
 import { delay } from "rxjs/operators";
+import {AbstractControl, FormGroup} from "@angular/forms";
 
-export interface PeriodicElements {
-  name:     string;
-  position: number;
-  weight:   number;
-  symbol:   string;
-}
-
-const ELEMENT_DATA: PeriodicElements[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-]
+import { ELEMENT_DATA } from "../shared/element_data";
 
 @Component({
   selector: 'app-dynamic-table',
@@ -30,6 +13,15 @@ const ELEMENT_DATA: PeriodicElements[] = [
   styleUrls: ['./dynamic-table.component.scss']
 })
 export class DynamicTableComponent implements OnInit {
+
+  feedbackForm: FormGroup;
+  // dataSource = new BehaviorSubject<AbstractControl[]>([]);
+  dataCapture;
+  elements: [];
+  editList;
+  comparingCount = 0;
+  dataObjectSize = 0;
+
 
   editableColumn = '';
   editableIndex = null;
