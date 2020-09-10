568dba880f9b7eca576b010d2442446c408e5cd2
tangani <tanganimoyo@gmail.com>
2020-09-10 20:18:54 +0200

    dynamic card for products

--- online-shop/src/app/home/home.component.ts
+++ online-shop/src/app/home/home.component.ts
@@ -13,6 +13,14 @@ export class HomeComponent implements OnInit {
   productCapture;
   dataSource;
 
+  defaultElevation = 2;
+  raisedElevation = 8;
+
+  titles:title[] = [{name:'title1'}, {name:'title2'},{name:'title3'}];
+  titleSelected;
+
+  name = 'Angular';
+
   constructor(
     private productsService: ProductsService,
     private formBuilder: FormBuilder
@@ -25,4 +33,18 @@ export class HomeComponent implements OnInit {
     })
   }
 
+
+  select(title) {
+    console.log(title);
+    // this.titles.map(t=>t.isSelected = false);
+    // title.isSelected = true;
+    // this.titleSelected = title;
+    // console.log(this.titles);
+  }
+
+}
+
+export class title {
+  name: string;
+  isSelected?: boolean;
 }
