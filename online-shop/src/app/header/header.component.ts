1da139a97907cd583e1370469743014cdf7407bb
tangani <tanganimoyo@gmail.com>
2020-09-11 10:37:34 +0200

    adding the dynamic search for data accessed through the API

--- online-shop/src/app/header/header.component.ts
+++ online-shop/src/app/header/header.component.ts
@@ -1,7 +1,12 @@
+// https://stackblitz.com/angular/gkjdbnlgddy?file=src%2Fapp%2Fautocomplete-auto-active-first-option-example.ts
+
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from "@angular/material/dialog";
 import { LoginComponent } from "../login/login.component";
 import { RegisterComponent } from "../register/register.component";
+import { Observable } from "rxjs";
+import { map, startWith } from "rxjs/operators";
+import { FormControl } from "@angular/forms";
 
 import { ProductsService } from "../services/products.service";
 
@@ -13,6 +18,9 @@ import { ProductsService } from "../services/products.service";
 export class HeaderComponent implements OnInit {
 
   dataSource;
+  formControl = new FormControl();
+  filterOptions: Observable<string[]>;
+  options: string[];
 
   constructor(
     private dialog: MatDialog,
@@ -22,7 +30,20 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
     this.productsService.getProducts().subscribe( (products) => {
       this.dataSource = products;
-    })
+      if (this.dataSource)
+      {
+        this.options = Object.keys(products).map(function (productIndex) {
+          let eachProduct = products[productIndex]["title"];
+          return eachProduct;
+        })
+        this.filterOptions = this.formControl.valueChanges.pipe(
+          startWith(''),
+          map(value => this._filter(value))
+        );
+      }
+    });
+
+
   }
 
   openLoginForm() {
@@ -31,6 +52,12 @@ export class HeaderComponent implements OnInit {
 
   openRegisterForm() {
     this.dialog.open(RegisterComponent, {width: '500px', height: '450px'})
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().indexOf(filterValue) === 0);
   }
 
 }
